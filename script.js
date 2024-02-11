41a961b3de56daa96f4ea810e7f7c4e4c17063a4
Ile16 <153200586+Ile16@users.noreply.github.com>
2024-02-12 00:16:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+/* esercizio 1 */
+let dataTypes = {
+    number: "comprendono numeri interi 1, 2, 3 ecc, oppure numeri con la virgola",
+    string: "corrisponde al testo all'interno di virgolette",
+    boolean: "confronto tra più condizioni che da come risultato vero o falso",
+    undefined: "quando ad esempio il nostro contenitore è vuoto e non è stato riempito con nessun ingrediente",
+    null: "quando il nostro contenitore viene svuotato",
+}  ;
+console.log(dataTypes);
+
+/* ESERCIZIO 2*/
+
+let obj = "Oggetto = un elenco di proprietà a cui vengono assegnati dei valori";
+console.log(obj);
+
+/* ESERCIZIO 3*/
+
+let numb1 = 12;
+let numb2 = 20;
+
+let addition = (numb1 + numb2);
+
+console.log(addition);
+
+/* ESERCIZIO 4*/
+
+let x = 12;
+
+/* ESERCIZIO 5*/
+
+let name = "Ilenia";
+
+/* ESERCIZIO 6*/
+
+let y = 4;
+let subtraction = (x - y);
+console.log(subtraction);
+
+/* ESERCIZIO 7*/
+
+let name1 = "john";
+let name2 = "John";
+
+name2 = name2.toLowerCase(name2);
+
+let check = (name1 == name2);
+
+console.log(check);
+
